9eb87b56de5dfcc28ea0737ff7ec10124dd00875
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-14 16:29:31 -0300

    Implementar factory para o Express

--- server/app.js
+++ server/app.js
@@ -1,17 +1,3 @@
-const express = require('express');
-const SomaController = require('./controller/SomaController');
-const SubtracaoController = require('./controller/SubtracaoController');
-const MultiplicacaoController = require('./controller/MultiplicacaoController');
-const DivisaoController = require('./controller/DivisaoController');
+const expressFactory = require('./service/factory/ExpressFactory');
 
-const app = express();
-
-app.use(express.json());
-app.use('/api/soma', SomaController);
-app.use('/api/subtracao', SubtracaoController);
-app.use('/api/multiplicacao', MultiplicacaoController);
-app.use('/api/divisao', DivisaoController);
-
-app.listen(3000, function () {
-    console.log("Servidor rodando na porta: %d", this.address().port);
-});
+expressFactory.iniciarServidor(process.env.PORT || 3000);
---
+++ server/service/factory/ExpressFactory.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const SomaController = require('../../controller/SomaController');
+const SubtracaoController = require('../../controller/SubtracaoController');
+const MultiplicacaoController = require('../../controller/MultiplicacaoController');
+const DivisaoController = require('../../controller/DivisaoController');
+
+class ExpressFactory {
+    static iniciarServidor(port = 3000) {
+        const app = express();
+
+        app.use(express.json());
+        app.use('/api/soma', SomaController);
+        app.use('/api/subtracao', SubtracaoController);
+        app.use('/api/multiplicacao', MultiplicacaoController);
+        app.use('/api/divisao', DivisaoController);
+
+        app.listen(port, function () {
+            console.log('Servidor rodando na porta: %d', this.address().port);
+        })
+    }
+}
+
+module.exports = ExpressFactory;
